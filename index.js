9f052a9a61a4d68e54612fd31e1d55fdc9e7bbe7
+ <+>
2023-10-02 18:35:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        const userId = data.id;
+        document.body.innerHTML = userId;
+      })
+      .catch(function (error) {
+        console.log(error);
+        document.body.innerHTML = error.message;
+      });
+  }
+  
+  // Example usage:
+  submitData("Steve", "steve@steve.com");
+  
